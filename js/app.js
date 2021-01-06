fe805f3857320035b1858e5ad9103e48ddb59c63
John Reed <contact.johnreed@gmail.com>
2021-01-05 20:39:02 -0500

    added eventListener to add modal cards on click

--- js/app.js
+++ js/app.js
@@ -47,12 +47,34 @@ function generateEmployees(data) {
 };
 
 let employeeCards = document.querySelectorAll('.card')
-console.log(employeeCards.length)
+//console.log(employeeCards.length)
 for (let i = 0; i < employeeCards.length; i++) {
   employeeCards[i].addEventListener('click', (event) => {
-    console.log(event.target.id)
-    if (i + 1 === event.target.id)
-    console.log(event.target.name.first)
+    let index = event.currentTarget.id
+    console.log(index)
+    let modalHtml = `
+      <div class="modal-container">
+          <div class="modal">
+              <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+              <div class="modal-info-container">
+                  <img class="modal-img" src="${data.results[i].picture.large}" alt="profile picture">
+                  <h3 id="name" class="modal-name cap">${data.results[i].name.first}</h3>
+                  <p class="modal-text">${data.results[i].email}</p>
+                  <p class="modal-text cap">${data.results[i].location.city}</p>
+                  <hr>
+                  <p class="modal-text">${data.results[i].phone}</p>
+                  <p class="modal-text">${data.results[i].location.street}, ${data.results[i].location.city}, ${data.results[i].location.state} 97204${data.results[i].location.postcode}</p>
+                  <p class="modal-text">Birthday: ${data.results[i].dob}</p>
+              </div>
+          </div>
+
+          <div class="modal-btn-container">
+              <button type="button" id="modal-prev" class="modal-prev btn">Prev</button>
+              <button type="button" id="modal-next" class="modal-next btn">Next</button>
+          </div>
+      </div>
+      `
+      gallery.insertAdjacentHTML('beforeend', modalHtml)
   })
 }
 
