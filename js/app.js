bc552b78faee336b499c82fccbfb31da4ed8e588
John Reed <contact.johnreed@gmail.com>
2021-01-08 14:54:36 -0500

    added keyup search function, it is narrowing down results, but not widening them when you delete letters from the search

--- js/app.js
+++ js/app.js
@@ -1,14 +1,3 @@
-//creates and appends search bar
-let search = document.querySelector('.search-container')
-let searchHtml = `
-  <form action="#" method="get">
-      <input type="search" id="search-input" class="search-input" placeholder="Search...">
-      <input type="submit" value="&#x1F50D;" id="search-submit" class="search-submit">
-  </form>
-  `
-search.insertAdjacentHTML('beforeend', searchHtml)
-
-
 
 const randomUserUrl = 'https://randomuser.me/api/?results=12'
 
@@ -20,24 +9,25 @@ async function getRandomEmployees(url) {
 }
 
 getRandomEmployees(randomUserUrl)
+  .then(data => data = data.results)
   .then(generateEmployees)
 
 
 
+
+
 function generateEmployees(data) {
-  //console.log(data.results.length)
   let gallery = document.getElementById('gallery')
-  for (let i = 0; i < data.results.length; i++) {
-    //console.log("hello")
+  for (let i = 0; i < data.length; i++) {
     let html = `
       <div class="card" id="${i}">
           <div class="card-img-container">
-              <img class="card-img" src="${data.results[i].picture.large}" alt="profile picture">
+              <img class="card-img" src="${data[i].picture.large}" alt="profile picture">
           </div>
           <div class="card-info-container">
-              <h3 id="name" class="card-name cap">${data.results[i].name.first} ${data.results[i].name.last}</h3>
-              <p class="card-text">${data.results[i].email}</p>
-              <p class="card-text cap">${data.results[i].location.city}, ${data.results[i].location.state}</p>
+              <h3 id="name" class="card-name cap">${data[i].name.first} ${data[i].name.last}</h3>
+              <p class="card-text">${data[i].email}</p>
+              <p class="card-text cap">${data[i].location.city}, ${data[i].location.state}</p>
           </div>
       </div>
       `
@@ -51,41 +41,21 @@ for (let i = 0; i < employeeCards.length; i++) {
     let index = event.currentTarget.id
     createModal(data, i)
 
-
-      //CHANGE gonna try dropping this into the createModal function
       let modalContainer = document.querySelector('.modal-container')
-      // let closeModal = document.querySelector('#modal-close-btn')
-      //
-      // closeModal.addEventListener('click', (event) =>{
-      //   modalContainer.remove()
-      // })
-
       let modalPrevButton = document.querySelector('#modal-prev')
       let modalNextButton = document.querySelector('#modal-next')
 
-      // modalPrevButton.addEventListener('click', (event) =>{
-      //   if (i > 0) {
-      //   modalContainer.remove()
-      //   createModal(data, i - 1)
-      //   }
-      // })
-      //
-      // modalNextButton.addEventListener('click', (event) =>{
-      //   if (i < 10) {
-      //   modalContainer.remove()
-      //   createModal(data, i + 1)
-      //   }
-      // })
   })
 }
+//TODO
+activeSearch(data)
 }
-
 
 
 
 function createModal (data, i) {
 
-  let unformatedPhone = data.results[i].phone
+  let unformatedPhone = data[i].phone
   let phone = unformatedPhone.replace(/\D/g, '')
   let formatedPhone = ''
   if (phone.length === 10){
@@ -101,18 +71,18 @@ function createModal (data, i) {
       <div class="modal">
           <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
           <div class="modal-info-container">
-              <img class="modal-img" src="${data.results[i].picture.large}" alt="profile picture">
-              <h3 id="name" class="modal-name cap">${data.results[i].name.first}</h3>
-              <p class="modal-text">${data.results[i].email}</p>
-              <p class="modal-text cap">${data.results[i].location.city}</p>
+              <img class="modal-img" src="${data[i].picture.large}" alt="profile picture">
+              <h3 id="name" class="modal-name cap">${data[i].name.first}</h3>
+              <p class="modal-text">${data[i].email}</p>
+              <p class="modal-text cap">${data[i].location.city}</p>
               <hr>
 
 
               <p class="modal-text">${formatedPhone}</p>
 
 
-              <p class="modal-text">${data.results[i].location.street.number} ${data.results[i].location.street.name}, ${data.results[i].location.state} ${data.results[i].location.postcode}</p>
-              <p class="modal-text">Birthday: ${data.results[i].dob.date.slice(5,7)+'/'+data.results[i].dob.date.slice(8,10)+'/'+data.results[i].dob.date.slice(0,4)}</p>
+              <p class="modal-text">${data[i].location.street.number} ${data[i].location.street.name}, ${data[i].location.state} ${data[i].location.postcode}</p>
+              <p class="modal-text">Birthday: ${data[i].dob.date.slice(5,7)+'/'+data[i].dob.date.slice(8,10)+'/'+data[i].dob.date.slice(0,4)}</p>
           </div>
       </div>
 
@@ -147,3 +117,58 @@ function createModal (data, i) {
     }
   })
 }
+
+
+//creates and appends search bar
+let search = document.querySelector('.search-container')
+let searchHtml = `
+  <form action="#" method="get">
+      <input type="search" id="search-input" class="search-input" placeholder="Search...">
+      <input type="submit" value="&#x1F50D;" id="search-submit" class="search-submit">
+  </form>
+  `
+search.insertAdjacentHTML('beforeend', searchHtml)
+
+let searchInput = document.getElementById('search-input')
+let searchSubmit = document.querySelector('.search-submit')
+
+
+
+function activeSearch(data) {
+  searchInput.addEventListener('keyup', (event) => {
+    let newList = []
+    let lowerCaseSearch = event.target.value.toLowerCase()
+    for (let i = 0; i < data.length; i++){
+      let name = `${data[i].name.first.toLowerCase()} ${data[i].name.last.toLowerCase()}`
+      if (name.includes(lowerCaseSearch)) {
+          newList.push(data[i])
+        }
+    }
+    let gallery = document.getElementById('gallery')
+    gallery.innerHTML = ''
+    console.log(newList)
+    generateEmployees(newList)
+  })
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//gotta get some space
