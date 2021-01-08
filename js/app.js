986a82b3dbc97f4af2ad2aa8821914f26705452c
John Reed <contact.johnreed@gmail.com>
2021-01-07 23:48:05 -0500

    refactored creating modal into a function and put eventListeners inside, all modal buttons now working

--- js/app.js
+++ js/app.js
@@ -10,7 +10,6 @@ search.insertAdjacentHTML('beforeend', searchHtml)
 
 
 
-
 const randomUserUrl = 'https://randomuser.me/api/?results=12'
 
 async function getRandomEmployees(url) {
@@ -22,7 +21,6 @@ async function getRandomEmployees(url) {
 
 getRandomEmployees(randomUserUrl)
   .then(generateEmployees)
-
 
 
 
@@ -51,106 +49,33 @@ let employeeCards = document.querySelectorAll('.card')
 for (let i = 0; i < employeeCards.length; i++) {
   employeeCards[i].addEventListener('click', (event) => {
     let index = event.currentTarget.id
-    //console.log(index)
-    //console.log(data.results)
-    let modalHtml = `
-      <div class="modal-container">
-          <div class="modal">
-              <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
-              <div class="modal-info-container">
-                  <img class="modal-img" src="${data.results[i].picture.large}" alt="profile picture">
-                  <h3 id="name" class="modal-name cap">${data.results[i].name.first}</h3>
-                  <p class="modal-text">${data.results[i].email}</p>
-                  <p class="modal-text cap">${data.results[i].location.city}</p>
-                  <hr>
-                  <p class="modal-text">${data.results[i].phone}</p>
-                  <p class="modal-text">${data.results[i].location.street}, ${data.results[i].location.city}, ${data.results[i].location.state} 97204${data.results[i].location.postcode}</p>
-                  <p class="modal-text">Birthday: ${data.results[i].dob.date.slice(5,7)+'/'+data.results[i].dob.date.slice(8,10)+'/'+data.results[i].dob.date.slice(0,4)}</p>
-              </div>
-          </div>
-
-          <div class="modal-btn-container">
-              <button type="button" id="modal-prev" class="modal-prev btn">Prev</button>
-              <button type="button" id="modal-next" class="modal-next btn">Next</button>
-          </div>
-      </div>
-      `
-      gallery.insertAdjacentHTML('beforeend', modalHtml)
+    createModal(data, i)
 
 
+      //CHANGE gonna try dropping this into the createModal function
       let modalContainer = document.querySelector('.modal-container')
-      console.log(modalContainer)
-      let closeModal = document.querySelector('#modal-close-btn')
-      console.log(closeModal)
-      closeModal.addEventListener('click', (event) =>{
-        console.log('hello')
-        modalContainer.remove()
-      })
+      // let closeModal = document.querySelector('#modal-close-btn')
+      //
+      // closeModal.addEventListener('click', (event) =>{
+      //   modalContainer.remove()
+      // })
 
       let modalPrevButton = document.querySelector('#modal-prev')
       let modalNextButton = document.querySelector('#modal-next')
-      modalPrevButton.addEventListener('click', (event) =>{
-        if (i > 0) {
-        modalContainer.remove()
-        let modalHtml = `
-          <div class="modal-container">
-              <div class="modal">
-                  <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
-                  <div class="modal-info-container">
-                      <img class="modal-img" src="${data.results[i - 1].picture.large}" alt="profile picture">
-                      <h3 id="name" class="modal-name cap">${data.results[i - 1].name.first}</h3>
-                      <p class="modal-text">${data.results[i - 1].email}</p>
-                      <p class="modal-text cap">${data.results[i - 1].location.city}</p>
-                      <hr>
-                      <p class="modal-text">${data.results[i - 1].phone}</p>
-                      <p class="modal-text">${data.results[i - 1].location.street}, ${data.results[i - 1].location.city}, ${data.results[i - 1].location.state} 97204${data.results[i - 1].location.postcode}</p>
-                      <p class="modal-text">Birthday: ${data.results[i - 1].dob.date.slice(5,7)+'/'+data.results[i - 1].dob.date.slice(8,10)+'/'+data.results[i - 1].dob.date.slice(0,4)}</p>
-                  </div>
-              </div>
 
-              <div class="modal-btn-container">
-                  <button type="button" id="modal-prev" class="modal-prev btn">Prev</button>
-                  <button type="button" id="modal-next" class="modal-next btn">Next</button>
-              </div>
-          </div>
-          `
-          gallery.insertAdjacentHTML('beforeend', modalHtml)
-        console.log('hello prev')
-        }
-      })
-
-      modalNextButton.addEventListener('click', (event) =>{
-        if (i < 10) {
-        modalContainer.remove()
-        let modalHtml = `
-          <div class="modal-container">
-              <div class="modal">
-                  <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
-                  <div class="modal-info-container">
-                      <img class="modal-img" src="${data.results[i + 1].picture.large}" alt="profile picture">
-                      <h3 id="name" class="modal-name cap">${data.results[i + 1].name.first}</h3>
-                      <p class="modal-text">${data.results[i + 1].email}</p>
-                      <p class="modal-text cap">${data.results[i + 1].location.city}</p>
-                      <hr>
-                      <p class="modal-text">${data.results[i + 1].phone}</p>
-                      <p class="modal-text">${data.results[i + 1].location.street}, ${data.results[i + 1].location.city}, ${data.results[i + 1].location.state} 97204${data.results[i + 1].location.postcode}</p>
-                      <p class="modal-text">Birthday: ${data.results[i + 1].dob.date.slice(5,7)+'/'+data.results[i + 1].dob.date.slice(8,10)+'/'+data.results[i + 1].dob.date.slice(0,4)}</p>
-                  </div>
-              </div>
-
-              <div class="modal-btn-container">
-                  <button type="button" id="modal-prev" class="modal-prev btn">Prev</button>
-                  <button type="button" id="modal-next" class="modal-next btn">Next</button>
-              </div>
-          </div>
-          `
-          gallery.insertAdjacentHTML('beforeend', modalHtml)
-        console.log('hello prev')
-        }
-      })
-
-
-
+      // modalPrevButton.addEventListener('click', (event) =>{
+      //   if (i > 0) {
+      //   modalContainer.remove()
+      //   createModal(data, i - 1)
+      //   }
+      // })
+      //
+      // modalNextButton.addEventListener('click', (event) =>{
+      //   if (i < 10) {
+      //   modalContainer.remove()
+      //   createModal(data, i + 1)
+      //   }
+      // })
   })
 }
 }
@@ -158,60 +83,67 @@ for (let i = 0; i < employeeCards.length; i++) {
 
 
 
+function createModal (data, i) {
+
+  let unformatedPhone = data.results[i].phone
+  let phone = unformatedPhone.replace(/\D/g, '')
+  let formatedPhone = ''
+  if (phone.length === 10){
+    formatedPhone = `(${phone.slice(0,3)}) ${phone.slice(3,6)}-${phone.slice(6,10)}`
+  } else if (phone.length === 11) {
+    formatedPhone = `+${phone[0]} (${phone.slice(1,4)}) ${phone.slice(4,7)}-${phone.slice(7,12)}`
+  } else {
+    formatedPhone = `${phone.slice(-8,-5)}-${phone.slice(-4)}`
+  }
+
+  let modalHtml = `
+  <div class="modal-container">
+      <div class="modal">
+          <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+          <div class="modal-info-container">
+              <img class="modal-img" src="${data.results[i].picture.large}" alt="profile picture">
+              <h3 id="name" class="modal-name cap">${data.results[i].name.first}</h3>
+              <p class="modal-text">${data.results[i].email}</p>
+              <p class="modal-text cap">${data.results[i].location.city}</p>
+              <hr>
 
 
-
-  // <div class="modal-container">
-  //     <div class="modal">
-  //         <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
-  //         <div class="modal-info-container">
-  //             <img class="modal-img" src="${data.results[i].picture.large}" alt="profile picture">
-  //             <h3 id="name" class="modal-name cap">${data.results[i].name.first}</h3>
-  //             <p class="modal-text">${data.results[i].email}</p>
-  //             <p class="modal-text cap">${data.results[i].location.city}</p>
-  //             <hr>
-  //             <p class="modal-text">${data.results[i].phone}</p>
-  //             <p class="modal-text">${data.results[i].location.street}, ${data.results[i].location.city}, ${data.results[i].location.state} 97204${data.results[i].location.postcode}</p>
-  //             <p class="modal-text">Birthday: ${data.results[i].dob}</p>
-  //         </div>
-  //     </div>
-  //
-  //     <div class="modal-btn-container">
-  //         <button type="button" id="modal-prev" class="modal-prev btn">Prev</button>
-  //         <button type="button" id="modal-next" class="modal-next btn">Next</button>
-  //     </div>
-  // </div>
+              <p class="modal-text">${formatedPhone}</p>
 
 
+              <p class="modal-text">${data.results[i].location.street.number} ${data.results[i].location.street.name}, ${data.results[i].location.state} ${data.results[i].location.postcode}</p>
+              <p class="modal-text">Birthday: ${data.results[i].dob.date.slice(5,7)+'/'+data.results[i].dob.date.slice(8,10)+'/'+data.results[i].dob.date.slice(0,4)}</p>
+          </div>
+      </div>
 
-// let cards = document.getElementById('gallery')
-// cards.addEventListener('click', (event) => {
-//   console.log(event.target.className)
-//   if (event.target.className.includes('card')) {
-//     console.log('hello if')
-//
-//   }
-// })
-  //element.insertAdjacentHTML('beforeend', 'HTML string')
+      <div class="modal-btn-container">
+          <button type="button" id="modal-prev" class="modal-prev btn">Prev</button>
+          <button type="button" id="modal-next" class="modal-next btn">Next</button>
+      </div>
+  </div>
+  `
+  gallery.insertAdjacentHTML('beforeend', modalHtml)
 
-  //
-  // <div class="modal-container">
-  //     <div class="modal">
-  //         <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
-  //         <div class="modal-info-container">
-  //             <img class="modal-img" src="${data.results[i].picture.large}" alt="profile picture">
-  //             <h3 id="name" class="modal-name cap">${data.results[i].name.first}</h3>
-  //             <p class="modal-text">${data.results[i].email}</p>
-  //             <p class="modal-text cap">${data.results[i].location.city}</p>
-  //             <hr>
-  //             <p class="modal-text">${data.results[i].phone}</p>
-  //             <p class="modal-text">${data.results[i].location.street}, ${data.results[i].location.city}, ${data.results[i].location.state} 97204${data.results[i].location.postcode}</p>
-  //             <p class="modal-text">Birthday: ${data.results[i].dob}</p>
-  //         </div>
-  //     </div>
-  //
-  //     <div class="modal-btn-container">
-  //         <button type="button" id="modal-prev" class="modal-prev btn">Prev</button>
-  //         <button type="button" id="modal-next" class="modal-next btn">Next</button>
-  //     </div>
-  // </div>
+  let modalContainer = document.querySelector('.modal-container')
+  let closeModal = document.querySelector('#modal-close-btn')
+
+  closeModal.addEventListener('click', (event) =>{
+    modalContainer.remove()
+  })
+  let modalPrevButton = document.querySelector('#modal-prev')
+  let modalNextButton = document.querySelector('#modal-next')
+
+  modalPrevButton.addEventListener('click', (event) =>{
+    if (i > 0) {
+    modalContainer.remove()
+    createModal(data, i - 1)
+    }
+  })
+
+  modalNextButton.addEventListener('click', (event) =>{
+    if (i < 11) {
+    modalContainer.remove()
+    createModal(data, i + 1)
+    }
+  })
+}
